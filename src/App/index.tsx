24d6097865027faf4da396c987c54288eb15bbc0
deokgoo <kkddgg1001@gmail.com>
2020-01-02 17:54:07 +0900

    init

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '../pages/Main';
+import './style.scss'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+};
+
+export default App;
