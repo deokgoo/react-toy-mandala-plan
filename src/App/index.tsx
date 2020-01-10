7dac438190a52aac8b4dc0b675fbca9fab508ff6
deok <kkddgg1001@gmail.com>
2020-01-10 18:20:22 +0900

    feature : add layout (MenuBar , setting)

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,7 +5,13 @@ import './style.scss'
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Main />
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          <div className="col-1 bg-dark h-100"></div>
+          <div className="col-7"><Main /></div>
+          <div className="col-4 bg-secondary h-100"></div>
+        </div>
+      </div>
     </div>
   );
 };
